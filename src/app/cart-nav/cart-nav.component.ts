8c06c50b4cbfa5c9fc7d749d7948e8da67935543
Nastya <a.dementeva@mashtab.org>
2018-12-26 19:24:58 +0300

    changed menu

--- src/app/cart-nav/cart-nav.component.ts
+++ src/app/cart-nav/cart-nav.component.ts
@@ -2,6 +2,12 @@ import { IData } from './../data/data';
 import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 
 
+enum MenuEnum {
+  'Все отели' = "all",
+  'Апартаменты' = "apartments",
+  'Номера люкс' = "lux"
+}
+
 @Component({
   selector: 'app-cart-nav',
   templateUrl: './cart-nav.component.html',
@@ -12,27 +18,26 @@ export class CartNavComponent  {
   @Input() public info: IData[];
   @Output() outputInfoHotel:EventEmitter<IData> = new EventEmitter();
 
-  public typeHotel: string = 'all';
+  public typeHotel: string = '';
   public infoHotels: IData[];
+  public menuList:Array<string> = ['Все отели','Апартаменты','Номера люкс'];
+  public menuEnum = MenuEnum;
 
   ngOnInit() {
-    this.navEnter('all');
+    this.navClick('Все отели');
   }
   
   public changeInfoHotel(item) {
-    item['active'] = true;
     this.outputInfoHotel.emit(item);
   }
 
-  public navEnter(type?: string) {
-    this.typeHotel = type;
-    if(this.typeHotel== 'all') {
+  public navClick(item: string) {
+    this.typeHotel = this.menuEnum[item];
+    if(this.typeHotel== this.menuEnum['Все отели']) {
       this.infoHotels = this.info;
     } else {
-      this.infoHotels = this.info.filter(res => res.type == type);
+      this.infoHotels = this.info.filter(res => res.type == this.typeHotel);
     }
     this.changeInfoHotel(this.infoHotels[0]);
   }
-  
-
 }
